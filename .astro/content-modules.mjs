a70d2f2c28b4bbce08c14ce4794065c0d2d588f2
Shinnken <konrad.suchodolski50@gmail.com>
2026-04-14 15:12:54 +0200

    Removing npm vunerabilities

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/lidar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Flidar.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/start.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fstart.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Ftest.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/examples/audio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fexamples%2Faudio.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/examples/movement.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fexamples%2Fmovement.mdx&astroContentModuleFlag=true")],
@@ -11,7 +11,7 @@ export default new Map([
 ["src/content/docs/guides/features/features.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Ffeatures%2Ffeatures.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/features/position-tool.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Ffeatures%2Fposition-tool.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/setup/installing-robocrew.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fsetup%2Finstalling-robocrew.mdx&astroContentModuleFlag=true")],
-["src/content/docs/guides/setup/raspberry-and-plug.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fsetup%2Fraspberry-and-plug.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/setup/udev-rules.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fsetup%2Fudev-rules.mdx&astroContentModuleFlag=true")],
+["src/content/docs/guides/setup/raspberry-and-plug.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fsetup%2Fraspberry-and-plug.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/setup/wifi-settings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Fsetup%2Fwifi-settings.mdx&astroContentModuleFlag=true")]]);
 		
